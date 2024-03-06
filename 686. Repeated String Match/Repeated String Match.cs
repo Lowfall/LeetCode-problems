2da3152d92b6d422e02b9efcee12ec2404ac67d9
Nikita Kyteyko <nikitakuteyko@gmail.com>
2024-03-06 21:06:17 +0300

    Solved problem №686

---
+++ 686. Repeated String Match/Repeated String Match.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class Repeated_String_Match
+    {
+        public static int RepeatedStringMatch(string a, string b)
+        {
+            string res = "";
+            int counter = 0;
+            while (res.Length < b.Length)
+            {
+                res += a;
+                counter++;
+            }
+            if (res.Contains(b))
+            {
+                return counter;
+            }
+            else if ((res + a).Contains(b))
+            {
+                return counter + 1;
+            }
+            return -1;
+        }
+    }
+}

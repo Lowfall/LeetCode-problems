70c15b6c3d697a3d13f6b3ff4f6bbcce2cb5846b
Nikita Kyteyko <nikitakuteyko@gmail.com>
2024-03-11 18:18:19 +0300

    Solved problem №706

---
+++ 706. Design HashMap/Design HashMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MyHashMap
+    {
+
+        private Dictionary<int, int> dict;
+
+        public MyHashMap()
+        {
+            dict = new Dictionary<int, int>();
+        }
+
+        public void Put(int key, int value)
+        {
+            if (dict.TryGetValue(key, out var elem))
+            {
+                dict[key] = value;
+            }
+            else dict.Add(key, value);
+        }
+
+        public int Get(int key)
+        {
+            if (dict.TryGetValue(key, out var elem)) return elem;
+            else return -1;
+        }
+
+        public void Remove(int key)
+        {
+            if (dict.TryGetValue(key, out var elem))
+            {
+                dict.Remove(key);
+            }
+        }
+    }
+}
